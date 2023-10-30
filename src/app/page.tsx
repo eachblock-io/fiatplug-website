9d6809424288441c955917ad4613840e05dc46f0
unknown <unclebignay@hashnode.com>
2023-10-30 18:08:09 +0100

    feat: services section and breadcrum section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import Breadcrum from "@/components/Breadcrum";
 import Hero from "@/components/Hero";
+import ServiceSection from "@/components/ServiceSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-amber-50">
       <Hero />
+      <Breadcrum />
+      <ServiceSection />
     </main>
   );
 }
